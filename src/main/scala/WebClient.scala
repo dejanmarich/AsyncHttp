486e9b913d63ca9044806bcf7145a2d0266e3610
dejanmarich <dejan.marich@gmail.com>
2019-03-05 14:39:56 +0100

    init commit

---
+++ src/main/scala/WebClient.scala
@@ -0,0 +1,27 @@
+import java.util.concurrent.Executor
+
+import com.ning.http.client.AsyncHttpClient
+
+import scala.concurrent.{Future, Promise}
+
+class WebClient {
+
+  private val client = new AsyncHttpClient()
+  def get(url: String)(implicit exec: Executor): Future[String] = {
+
+    val f = client.prepareGet(url).execute()
+    val p = Promise[String]()
+    f.addListener(new Runnable {
+      def run = {
+        val response = f.get
+        if (response.getStatusCode < 400)
+          p.success(response.getResponseBodyExcerpt(131072))
+        else p.failure(BadStatus(response.getStatusCode))
+      }
+    }, exec)
+    p.future
+
+  }
+
+
+}
